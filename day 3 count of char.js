7fb88ecddbe42d233ca42e07e898e8178e15cab9
Sai Aditya Devulapalli <dsaiaditya@gmail.com>
2024-04-12 15:46:15 +0530

    file renaming

--- day 3 count of char.js
+++ day 3 count of char.js
@@ -1,6 +1,6 @@
-//*
+//* ------------------------------------------------------
 //* Interview Question: Count Occurrences of Character
-//*
+//* ------------------------------------------------------
 //! Task:
 //? Write a function called countChar that takes two parameters: a string and a character to count. The function should return the number of times the specified character appears in the string.
 
